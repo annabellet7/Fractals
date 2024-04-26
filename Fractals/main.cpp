8aea5a98435ebbf46a5e343d58b55374167d0397
annabellet7 <annabelle.thompson@mymail.champlain.edu>
2024-04-25 22:11:28 -0400

    added swiching between fractals

--- Fractals/main.cpp
+++ Fractals/main.cpp
@@ -15,23 +15,39 @@ int main(int argc, char* argv[])
 	SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, 0);
 	
 
-	int xMouse, yMouse;
+	int xMouse, yMouse, state = 1;
 	double r, i;
 	double real, imaginary;
 	SDL_Event event;
 
+	mandelbrot.drawMandelbrot(renderer);
+
 	std::cout << "running" << std::endl;
 	while (running)
 	{
-		//SDL_RenderClear(renderer);
-		//mandelbrot.drawMandelbrot(renderer);
-		julia.drawJulia(renderer);
+		if (state == 2)
+		{
+			julia.drawJulia(renderer);
+		}
 		while (SDL_PollEvent(&event))
 		{
 			switch (event.type)
 			{
 			case SDL_KEYDOWN:
-				running = false;
+				switch (event.key.keysym.sym)
+				{
+				case SDLK_ESCAPE:
+					running = false;
+					break;
+				case SDLK_1:
+					mandelbrot.drawMandelbrot(renderer);
+					state = 1;
+					break;
+				case SDLK_2:
+					julia.drawJulia(renderer);
+					state = 2;
+					break;
+				}
 				break;
 			case SDL_MOUSEMOTION:
 				SDL_GetMouseState(&xMouse, &yMouse);
@@ -55,10 +71,11 @@ int main(int argc, char* argv[])
 			}
 		}
 		SDL_RenderPresent(renderer);
-		//SDL_Delay(16);
+		SDL_Delay(16);
 	}
 
 	SDL_DestroyRenderer(renderer);
 	SDL_DestroyWindow(window);
+	SDL_Quit();
 	return 0;
 }
